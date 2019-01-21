5a83e272eae7c12a78ec362b661ae1b46c21fb7f
d.a.golovachev <d.a.golovachev@gmail.com>
2019-01-21 21:41:55 +0600

    Refactored

--- Test/Program.cs
+++ Test/Program.cs
@@ -11,30 +11,14 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            // var client = new Chat2DeskClient("97d35f4783c70c8b7412a3068cc43b"); // imarat
-            var client = new Chat2DeskClient("d1bdb8e80fc2c4d3050d49a10a433d");// crm
 
+            var client = new Chat2DeskClient("token");
+            var messages = client.GetMessages(2550);
 
-            var response = client.CreateClient("996554644662", Transport.Whatsapp, 0, "Дмитрий Головачев");
-
-            if (response.Status == ResponseStatus.Success)
+            foreach (var message in messages.Data)
             {
-                Console.WriteLine(response.Data.Id + " " + response.Data.AssignedName);
-                var messageReponse = client.SendMessage(response.Data.Id, "TEST123", Transport.Whatsapp);
-                if (messageReponse.Status == ResponseStatus.Success) Console.WriteLine(messageReponse.Data.MessageId);
-                else Console.WriteLine("all bad");
+                Console.WriteLine($"ID: {message.Id}, Text:{message.Text}, Transport: {message.Transport.GetStringValue()}");
             }
-            else Console.WriteLine("bad");
-
-
-
-            //  var client = new Chat2DeskClient("97d35f4783c70c8b7412a3068cc43b");
-            //var messages = client.GetMessages(2550);
-
-            //foreach (var message in messages.Data)
-            //{
-            //    Console.WriteLine($"ID: {message.Id}, Text:{message.Text}, Transport: {message.Transport.GetStringValue()}");
-            //}
 
             Console.ReadKey();
         }
